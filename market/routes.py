5edd68fd4e5f190befa8c58a5cee8803eade2ea3
Sneha1203 <78474043+Sneha1203@users.noreply.github.com>
2022-05-13 23:26:52 +0530

    final commit

--- market/routes.py
+++ market/routes.py
@@ -1,8 +1,10 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, request
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
 from market import db
+from flask_login import login_user, logout_user, login_required, current_user
+
 
 @app.route('/')
 @app.route('/home')
@@ -10,10 +12,36 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/market')
+@app.route('/market', methods=["GET", "POST"])
+@login_required
 def market_page():
-    items = Item.query.all()
-    return render_template('market.html', items=items)
+    purchase_form = PurchaseItemForm()
+    sell_form = SellItemForm()
+    if request.method == 'POST':
+
+        purchased_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            if current_user.can_purchase(p_item_object):
+                p_item_object.buy(current_user)
+                flash(f'Congratulations! You purchased {p_item_object.name} for $ {p_item_object.price}!', category='success')
+            else:
+                flash(f'Unfortunately, you do not have enough money to purchase {p_item_object.name}.', category='error')
+
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(f'Congratulations! You sold {s_item_object.name} back to market!', category='success')
+            else:
+                flash(f'Something went wrong with selling {s_item_object.name}.', category='error')
+
+        return redirect(url_for('market_page'))
+    if request.method == 'GET':
+        items = Item.query.filter_by(owner=None)
+        owned_items = Item.query.filter_by(owner=current_user.id)
+        return render_template('market.html', items=items, purchase_form=purchase_form, owned_items=owned_items, sell_form=sell_form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -25,8 +53,34 @@ def register():
                             password=form.password1.data)
         db.session.add(create_user)
         db.session.commit()
+        login_user(create_user)
+        flash(f'Account created successfully! You are logged in as {create_user.username}!', category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}:               # if there are errors from the validations
         for err_msg in form.errors.values():
-            print(f'There was an error with creating user: {err_msg}')
+            flash(f'There was an error with creating user: {err_msg}', category='danger')
     return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password(attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'You have logged in successfully! Welcome {attempted_user.username}!', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash(f'Username and password do not match! Please try again!', category='danger')
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash(f'You have been logged out successfully!', category='info')
+    return redirect(url_for('home'))
+
+
+
